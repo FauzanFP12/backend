fa5c347a84fc07e1e30c78f663dcc20c67f8449d
FauzanFP12 <120654283+FauzanFP12@users.noreply.github.com>
2024-10-05 23:47:53 +0700

    commit

--- controllers/insidenController.js
+++ controllers/insidenController.js
@@ -16,10 +16,12 @@ export const createInsiden = async (req, res) => {
 
   const { idInsiden, deskripsi, status, tanggalStart, tanggalSubmit, sbu, backbone, superbackbone, distribusi, access, pilihan } = req.body;
 
-  // Adjust the submitted 'tanggalStart' time to GMT+7
-  const utcTime = new Date(tanggalStart); // Original UTC time from request
-  const gmt7Time = new Date(utcTime.getTime() + 7 * 60 * 60 * 1000); // Convert UTC to GMT+7
-
+  if (!tanggalStart || isNaN(Date.parse(tanggalStart))) {
+    return res.status(400).json({ message: 'Invalid date format for tanggalStart' });
+  }
+  
+  const utcTime = new Date(tanggalStart); 
+  const gmt7Time = new Date(utcTime.getTime() + 7 * 60 * 60 * 1000);
   const newInsiden = new Insiden({
     idInsiden,
     deskripsi,
