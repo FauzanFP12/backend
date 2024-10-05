eb4b2fc99c28501ffb238802fbdbe550b7872181
FauzanFP12 <120654283+FauzanFP12@users.noreply.github.com>
2024-10-06 02:54:27 +0700

    close

--- routes/insidenRoutes.js
+++ routes/insidenRoutes.js
@@ -11,7 +11,6 @@ const router = express.Router();
 // Routes for incidents
 router.get('/', getInsidens); // GET all incidents
 router.post('/', createInsiden); // POST a new incident
-router.put('/:id', updateInsiden); // UPDATE an incident
 router.put('/close/:id', closeInsiden);// Close an incident
 router.put('/reopen/:id', reopenInsiden);// Reopen an incident
 router.delete('/:id', deleteInsiden); // DELETE an incident
