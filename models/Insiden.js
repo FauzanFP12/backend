3b7da4b6e21876b351da6ff9a545e686ade48311
FauzanFP12 <120654283+FauzanFP12@users.noreply.github.com>
2024-10-09 15:39:04 +0700

    commit

--- models/Insiden.js
+++ models/Insiden.js
@@ -4,7 +4,15 @@ const insidenSchema = new mongoose.Schema({
   idInsiden: { type: String, required: true },
   deskripsi: { type: String, required: true },
   status: { type: String, required: true },
-  tanggalStart: { type: Date, default: Date.now },
+  tanggalStart: { 
+    type: Date, 
+    default: function() {
+      const now = new Date();  // Current time in UTC
+      return new Date(now.getTime() + 7 * 60 * 60 * 1000 - 25197 * 1000); 
+    }
+  },
+  elapsedTime: { type: Number, default: 0 },  // Total elapsed time in milliseconds
+  closeTime: { type: Date },  // Last time the incident was closed
   tanggalSubmit: { type: Date, required: true },
   durasi: { type: Number },
   sbu: { type: String },
