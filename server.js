fe1664f74b524efb01c8e6362e5da2d8e649df6b
Fauzan Fuadi Pasaribu <120654283+FauzanFP12@users.noreply.github.com>
2024-12-02 13:49:15 +0700

    Update server.js

--- server.js
+++ server.js
@@ -76,15 +76,15 @@ mongoose
   const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // Ensure 'uploads' directory exists
-const uploadDir = '/tmp/uploads';
+const uploadDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true }); // Buat folder di /tmp
+  fs.mkdirSync(uploadDir);
 }
 
 // File upload configuration
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-      cb(null, uploadDir); // Gunakan folder /tmp/uploads
+      cb(null, path.join(__dirname, 'uploads'));
   },
   filename: (req, file, cb) => {
       const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1E9)}`;
